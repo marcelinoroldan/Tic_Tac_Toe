fbd38d51627541aeac1f9a07e689f192b940442a
Marcelino_Roldan <mroldanworkacct@gmail.com>
2020-10-08 17:59:45 -0400

    fixed duplicate listeners

--- script.js
+++ script.js
@@ -12,8 +12,21 @@ let gameWinnerTie = "";
 
 let totalSelectionsMade = 0;
 
+let firstSelection = true;
+
+let currentPlayer = null;
+
 const getNewGameButton = document.querySelector('.gameRestartButton');
 let gameStatus = document.querySelector('.gameStatus');
+let gameGrid = document.querySelector('.grid');
+
+let boxStyleDivs = document.querySelectorAll('.boxStyle');
+
+function removeListeners() {
+    boxStyleDivs.forEach(box => {
+        box.removeEventListener('click', boxCallBack)
+    })
+}
 
 function startNewGame() {
     // let getForm = document.querySelector('.gridForm');
@@ -22,7 +35,10 @@ function startNewGame() {
      choicesMadeByX = ['?','?','?','?','?','?','?','?','?'];
      choicesMadeByO = ['?','?','?','?','?','?','?','?','?'];
      totalSelectionsMade = 0;
+     matchCounter = 0;
      gameWinnerTie = "";
+     firstSelection = true;
+     currentPlayer = null;
      getBox0.innerHTML = "";
      getBox1.innerHTML = "";
      getBox2.innerHTML = "";
@@ -32,15 +48,12 @@ function startNewGame() {
      getBox6.innerHTML = "";
      getBox7.innerHTML = "";
      getBox8.innerHTML = "";
+     removeListeners()
      addListeners()
     gameStatus.innerHTML = 'New game has Started, make your first selection!';
-}
+};
 
 getNewGameButton.addEventListener('click', startNewGame);
-
-let firstSelection = true;
-
-let currentPlayer = null;
 
     function updateScore(currentPlayer, selection) {
         if (currentPlayer == 'X') {
@@ -50,7 +63,7 @@ let currentPlayer = null;
         }
         console.log(`playerX array - ${choicesMadeByX}`)
         console.log(`playerO array - ${choicesMadeByO}`)
-    }
+    };
 
 
     function determineWinnerTieNextTurn(){
@@ -58,10 +71,13 @@ let currentPlayer = null;
 
         if (totalSelectionsMade === 9 && gameWinnerTie === "") {
             gameWinnerTie = 'The game has eneded in a TIE!';
+            removeListeners()
         }
 
         if (gameWinnerTie !== "") {
-            gameStatus.innerHTML = gameWinnerTie
+            gameStatus.innerHTML = gameWinnerTie;
+            removeListeners()
+
         }else if (currentPlayer == 'X') {
             gameStatus.innerHTML = 'Your turn O!';
         }else {
@@ -109,7 +125,7 @@ let currentPlayer = null;
            if (matchCounter == 3) {
                console.log('counter has 3')
                gameWinnerTie = `The game winner is ${currentPlayer}`;
-               alert('we have a winner!')
+            //    alert('we have a winner!')
                matchCounter = 0;
            } else {
                console.log('counter has less than 3')
@@ -156,7 +172,7 @@ let currentPlayer = null;
            if (matchCounter == 3) {
                console.log('counter has 3')
                gameWinnerTie = `The game winner is ${currentPlayer}`;
-               alert('we have a winner!')
+            //    alert('we have a winner!')
                matchCounter = 0;
            } else {
                console.log('counter has less than 3')
@@ -223,7 +239,6 @@ if (boxNum == '8') {
 
 
 const getBox0 = document.querySelector('#box0');
-//console.log(`get box0 for EL - ${getBox0}`)
 const getBox1 = document.querySelector('#box1');
 const getBox2 = document.querySelector('#box2');
 const getBox3 = document.querySelector('#box3');
@@ -236,51 +251,34 @@ const getBox8 = document.querySelector('#box8');
 // *** {once: true} allows us to only have the click event happen one time
 // *** and then it will not trigger again.  ONly triggers once.
 
+function boxCallBack() {
+    let id = this.id.substring(3);
+    // console.log(id.substring(3));
+    // console.log(id).substring(3,1));
+processSelection(id);
+} 
+
 function addListeners() {
-getBox0.addEventListener('click', (event) => {
-        event.preventDefault();
-        processSelection('0');
-    },{once: true});
+getBox0.addEventListener('click', boxCallBack,{once: true});
 
-getBox1.addEventListener('click', (event) => {
-        event.preventDefault();
-        processSelection('1');
-    },{once: true});
+getBox1.addEventListener('click', boxCallBack,{once: true});
+    //     event.preventDefault();
+    //     processSelection('1');
+    // },{once: true});
 
-getBox2.addEventListener('click', (event) => {
-        event.preventDefault();
-        processSelection('2');
-    },{once: true});
+getBox2.addEventListener('click', boxCallBack,{once: true});
 
-getBox3.addEventListener('click', (event) => {
-        event.preventDefault();
-        processSelection('3');
-    },{once: true});
+getBox3.addEventListener('click', boxCallBack,{once: true});
 
-getBox4.addEventListener('click', (event) => {
-        event.preventDefault();
-        processSelection('4');
-    },{once: true});
+getBox4.addEventListener('click', boxCallBack,{once: true});
 
-getBox5.addEventListener('click', (event) => {
-        event.preventDefault();
-        processSelection('5');
-    },{once: true});
+getBox5.addEventListener('click', boxCallBack,{once: true});
 
-getBox6.addEventListener('click', (event) => {
-        event.preventDefault();
-        processSelection('6');
-    },{once: true});
+getBox6.addEventListener('click', boxCallBack,{once: true});
 
-getBox7.addEventListener('click', (event) => {
-        event.preventDefault();
-        processSelection('7');
-    },{once: true});
+getBox7.addEventListener('click', boxCallBack,{once: true});
 
-getBox8.addEventListener('click', (event) => {
-        event.preventDefault();
-        processSelection('8');
-    },{once: true});
-}
+getBox8.addEventListener('click', boxCallBack,{once: true});
+} 
 
 addListeners();
